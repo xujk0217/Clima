84c2423cbe3cb8f093d96d86192ac9a3cd64cf66
許君愷 / xu jun kai <kevin16021777@gmail.com>
2024-06-19 21:28:34 +0800

    feat: get input 1. UITextFieldDelegate learning 2. learning Protocol and Delegate

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,48 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    var weatherManager = WeatherManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchTextField.delegate = self
+        //let ViewController to see textField delegate
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        //call should end editing
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //call should end editing
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        //scan if edit mode need to end or not
+        if textField.text != ""{
+            return true
+        }else{
+            textField.placeholder = "type something"
+            return false
+        }
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) { //did when ebd editing
+        // use searchTextField.text to get the weather for that city
+        if let city = searchTextField.text{
+            weatherManager.fetchWeather(cityName: city)
+        }
+        
+        searchTextField.text = ""
+    }
 }
 
